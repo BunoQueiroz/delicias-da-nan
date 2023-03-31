d68a4d7969dc5b70740e4500b763339eb79779d1
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-31 16:45:00 -0300

    Adicionando os modelos de prato e bebida ao admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from core.models import Dish, Drink
 
-# Register your models here.
+class DishAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'price', 'serves', 'category']
+    list_editable = ['name', 'description', 'price']
+    list_display_links = ['id',]
+    list_filter = ['category', 'serves']
+    search_fields = ['name', 'description']
+
+class DrinkAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'price', 'category']
+    list_editable = ['name', 'description', 'price']
+    list_display_links = ['id',]
+    list_filter = ['category']
+    search_fields = ['name', 'description']
+
+admin.site.register(Dish, DishAdmin)
+admin.site.register(Drink, DrinkAdmin)
