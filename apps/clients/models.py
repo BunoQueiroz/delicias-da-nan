64ba146d497abdfd1a6569c4f9d9c2828261bd71
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-31 16:40:59 -0300

    Desenvolvendo os modelos de cliente e pedido

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from core.models import Product
 
-# Create your models here.
+class Client(User):
+    rg = models.CharField(max_length=11, blank=True)
+    cpf = models.CharField(max_length=11, blank=True)
+    phone = models.CharField(max_length=11, blank=True)
+    image = models.ImageField(upload_to='clients/%Y/%m/%d/', blank=True)
+    date_of_birth = models.DateField(default='', blank=True)
+
+    def __str__(self):
+        return self.get_full_name()
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    amount = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.client.first_name} - {self.product} - {self.amount}'
+
