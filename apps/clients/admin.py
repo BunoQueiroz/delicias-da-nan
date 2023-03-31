42f4c3a6ea856546830beb671e1469b992e426b1
Bruno_Castro_Queiroz <bs8872491@gmail.com>
2023-03-31 16:43:46 -0300

    Adicionando cliente e pedido ao admin

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from clients.models import Client, Order
 
-# Register your models here.
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['cpf', 'first_name', 'email', 'phone']
+    list_editable = ['first_name',]
+    list_display_links = ['cpf',]
+    list_per_page = 10
+    list_filter = ['first_name',]
+    search_fields = ['first_name', 'last_name']
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['client', 'product', 'amount']
+    list_editable = ['amount',]
+    list_display_links = ['client',]
+    list_filter = ['client',]
+    search_fields = ['product']
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Order, OrderAdmin)
